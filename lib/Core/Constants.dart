18df85893fb2f2fa290010abf234e475f410e23b
Nora11gabr <noragabr48@gmail.com>
2024-01-22 10:40:03 +0200

    Logo

--- lib/Core/Constants.dart
+++ lib/Core/Constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String logo = 'assets/images/virus.png';
+String logo = 'assets/images/LOGO.png';
 Color Kprimarycolor = Color(0xff6f9a61);
 Color Ksecondarycolor = Color(0xfff5f5f5);
 Color Asecondarycolor = Color(0xff858484);
--- lib/Features/Bottom_nav/Presentation/BottomNav.dart
+++ lib/Features/Bottom_nav/Presentation/BottomNav.dart
@@ -19,7 +19,7 @@ class _BottomnavState extends State<Bottomnav> {
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   static const List<Widget> _widgetOptions = <Widget>[
     HomeView(),
-    ChatView(),
+    ServicesView(),
     CommunityView(),
     ProfileView(),
   ];
